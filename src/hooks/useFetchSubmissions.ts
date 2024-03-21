429402a75ace5969cb522664aa568a3751953c04
codebysid <codebysidd@gmail.com>
2024-03-21 16:54:47 +0530

    null

--- src/hooks/useFetchSubmissions.ts
+++ src/hooks/useFetchSubmissions.ts
@@ -7,7 +7,7 @@ const useFetchSubmissions = () => {
     try {
       const res = await fetch(`${process.env.REACT_APP_NODE_APP_URL}/api/codesubmission/read`)
       const data = await res.json()
-      if (!data.data) return
+      if (!data.data) return null
       setCodeSubmissions(data.data)
     } catch (err) {
       console.log(err)
